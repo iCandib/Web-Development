cf4371db0c5e26cf2c1918e2cdff0204dde9644d
Isaac Candib <isaaccandib@gmail.com>
2023-07-11 14:17:24 -0400

    javascript updates

--- mollie_site/index.js
+++ mollie_site/index.js
@@ -1,6 +1,29 @@
+// const express = require('express');
+// const bodyParser = require('body-parser');
+
+// const app = express();
+// const port = 4220; // Choose a port number
+
+// // Serve static files from the current directory
+// app.use(express.static(__dirname));
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+
+// app.get("/", function (req, res) {
+//     res.sendFile(__dirname + '/index.html');
+// });
+
+
+
+// // Start the server
+// app.listen(port, () => {
+//     console.log(`Server running at http://localhost:${port}/`);
+// });
+
+
 const express = require('express');
-const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
 
 const app = express();
 const port = 4220; // Choose a port number
@@ -8,7 +31,6 @@ const port = 4220; // Choose a port number
 // Serve static files from the current directory
 app.use(express.static(__dirname));
 app.use(bodyParser.urlencoded({ extended: true }));
-
 
 app.get("/", function (req, res) {
     res.sendFile(__dirname + '/index.html');
