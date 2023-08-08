a7e66b74485ef5678265a90fdee565158e9652f6
Isaac Candib <isaaccandib@gmail.com>
2023-08-08 17:50:32 -0400

    consultation page scripts

--- mollie_site/script.js
+++ mollie_site/script.js
@@ -91,7 +91,7 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 
-// JotForm - Consultation Page
+// JotForm - Contact Page
 JotForm.newDefaultTheme = true;
 JotForm.extendsNewTheme = false;
 JotForm.singleProduct = false;
@@ -154,3 +154,220 @@ setTimeout(function () {
 }, 20);
 
 
+// Jotform Consultation Page
+JotForm.newDefaultTheme = true;
+JotForm.extendsNewTheme = false;
+JotForm.singleProduct = false;
+JotForm.newPaymentUIForNewCreatedForms = true;
+JotForm.newPaymentUI = true;
+JotForm.clearFieldOnHide = "disable";
+JotForm.submitError = "jumpToFirstError";
+
+JotForm.init(function () {
+	/*INIT-START*/
+	JotForm.setPhoneMaskingValidator(
+		"input_5_full",
+		"\u0028\u0023\u0023\u0023\u0029 \u0023\u0023\u0023\u002d\u0023\u0023\u0023\u0023"
+	);
+	if (window.JotForm && JotForm.accessible)
+		$("input_12").setAttribute("tabindex", 0);
+	if (window.JotForm && JotForm.accessible)
+		$("input_6").setAttribute("tabindex", 0);
+	if (window.JotForm && JotForm.accessible)
+		$("input_11").setAttribute("tabindex", 0);
+	if (window.JotForm && JotForm.accessible)
+		$("input_9").setAttribute("tabindex", 0);
+	JotForm.alterTexts(undefined);
+	/*INIT-END*/
+});
+
+setTimeout(function () {
+	JotForm.paymentExtrasOnTheFly([
+		null,
+		null,
+		{
+			name: "submit2",
+			qid: "2",
+			text: "Submit",
+			type: "control_button",
+		},
+		{
+			description: "",
+			name: "name",
+			qid: "3",
+			text: "Name",
+			type: "control_fullname",
+		},
+		{
+			description: "",
+			name: "email",
+			qid: "4",
+			subLabel: "",
+			text: "Email",
+			type: "control_email",
+		},
+		{
+			description: "",
+			name: "phoneNumber",
+			qid: "5",
+			text: "Phone Number",
+			type: "control_phone",
+		},
+		{
+			description: "",
+			name: "pleaseShare",
+			qid: "6",
+			subLabel: "",
+			text: "Please share your reason(s) for seeking therapy at this time",
+			type: "control_textarea",
+		},
+		null,
+		null,
+		{
+			description: "",
+			name: "howDid",
+			qid: "9",
+			subLabel: "",
+			text: "How did you hear about Mollie Spiesman, LCSW?",
+			type: "control_textbox",
+		},
+		null,
+		{
+			description: "",
+			name: "soThat",
+			qid: "11",
+			subLabel: "",
+			text: "So that we can schedule a complimentary 15-minute consultation, please list your availability over the next week. I will reach out within 24 business hours to confirm a time. ",
+			type: "control_textarea",
+		},
+		{
+			description: "",
+			name: "inWhat",
+			qid: "12",
+			subLabel: "",
+			text: "In what state do you currently live?",
+			type: "control_textbox",
+		},
+	]);
+}, 20);
+
+// Jotform Supervision Consultation Page
+JotForm.newDefaultTheme = true;
+JotForm.extendsNewTheme = false;
+JotForm.singleProduct = false;
+JotForm.newPaymentUIForNewCreatedForms = true;
+JotForm.newPaymentUI = true;
+JotForm.clearFieldOnHide = "disable";
+JotForm.submitError = "jumpToFirstError";
+JotForm.init(function () {
+	/*INIT-START*/
+	if (window.JotForm && JotForm.accessible)
+		$("input_19").setAttribute("tabindex", 0);
+	JotForm.setPhoneMaskingValidator(
+		"input_5_full",
+		"\u0028\u0023\u0023\u0023\u0029 \u0023\u0023\u0023\u002d\u0023\u0023\u0023\u0023"
+	);
+	if (window.JotForm && JotForm.accessible)
+		$("input_12").setAttribute("tabindex", 0);
+	if (window.JotForm && JotForm.accessible)
+		$("input_18").setAttribute("tabindex", 0);
+	if (window.JotForm && JotForm.accessible)
+		$("input_6").setAttribute("tabindex", 0);
+	if (window.JotForm && JotForm.accessible)
+		$("input_11").setAttribute("tabindex", 0);
+	if (window.JotForm && JotForm.accessible)
+		$("input_9").setAttribute("tabindex", 0);
+	JotForm.alterTexts(undefined);
+	/*INIT-END*/
+});
+
+setTimeout(function () {
+	JotForm.paymentExtrasOnTheFly([
+		null,
+		null,
+		{
+			name: "submit2",
+			qid: "2",
+			text: "Submit",
+			type: "control_button",
+		},
+		{
+			description: "",
+			name: "name",
+			qid: "3",
+			text: "Name",
+			type: "control_fullname",
+		},
+		{
+			description: "",
+			name: "email",
+			qid: "4",
+			subLabel: "",
+			text: "Email",
+			type: "control_email",
+		},
+		{
+			description: "",
+			name: "phoneNumber",
+			qid: "5",
+			text: "Phone Number",
+			type: "control_phone",
+		},
+		{
+			description: "",
+			name: "pleaseShare",
+			qid: "6",
+			subLabel: "",
+			text: "Please share your reason(s) for seeking supervision at this time.",
+			type: "control_textarea",
+		},
+		null,
+		null,
+		{
+			description: "",
+			name: "howDid",
+			qid: "9",
+			subLabel: "",
+			text: "How did you hear about Mollie Spiesman, LCSW?",
+			type: "control_textbox",
+		},
+		null,
+		{
+			description: "",
+			name: "soThat",
+			qid: "11",
+			subLabel: "",
+			text: "So that we can schedule a brief call, please list your availability over the next week. I will reach out within 24 business hours to confirm a time. ",
+			type: "control_textarea",
+		},
+		{
+			description: "",
+			name: "inWhat",
+			qid: "12",
+			subLabel: "",
+			text: "In what state(s) are you licensed?",
+			type: "control_textbox",
+		},
+		null,
+		null,
+		null,
+		null,
+		null,
+		{
+			description: "",
+			name: "whereDo",
+			qid: "18",
+			subLabel: "",
+			text: "Where do you currently work? Please provide a website if applicable.",
+			type: "control_textarea",
+		},
+		{
+			description: "",
+			name: "licenseCredentials",
+			qid: "19",
+			subLabel: "",
+			text: "License Credentials",
+			type: "control_textbox",
+		},
+	]);
+}, 20);
